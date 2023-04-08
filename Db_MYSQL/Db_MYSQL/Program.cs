8645d1ba134954b9933632d0c07e0302213290a7
DMzartex <190478@site.asty-moulin.be>
2023-04-09 00:49:33 +0200

    ajout ex db

---
+++ Db_MYSQL/Db_MYSQL/Program.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Db_MYSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlDataReader result;
+            string resultInfo;
+            int numberProduct;
+            string nameProduct;
+            int idUser;
+            string nameUser;
+
+            Console.WriteLine("SELECT USERS");
+            Query mesRequetes = new Query();
+            result = mesRequetes.SelectUser();
+            
+            while (result.Read())
+            {
+                Console.WriteLine(result["name"].ToString());
+            }
+            resultInfo = mesRequetes.DeleteProduct();
+            Console.WriteLine(resultInfo);
+
+            Console.WriteLine("Entrer le number de votre produit :");
+            numberProduct = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrer le nom de votre produit :");
+            nameProduct = Console.ReadLine();
+
+            resultInfo = mesRequetes.AddProduct(numberProduct, nameProduct);
+            Console.WriteLine(resultInfo);
+
+            Console.WriteLine("Entrer l'id de l'utilisateur pour modifier son nom :");
+            idUser = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrer le nouveau nom de l'utilisateur :");
+            nameUser = Console.ReadLine();
+
+            resultInfo = mesRequetes.ModifNameUserById(nameUser, idUser);
+
+            Console.WriteLine(resultInfo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Db_MYSQL/Db_MYSQL/Query.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace Db_MYSQL
+{
+    internal struct Query
+    {
+        public string DefinirChemin()
+        {
+            return "Server=localhost;User Id=root;password=;Database=dbvisualstudio;Port=3306";
+        }
+
+
+        public MySqlDataReader SelectUser()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(DefinirChemin());
+                string querySelectUser = "SELECT * FROM users WHERE idUser = 1";
+                MySqlCommand command = new MySqlCommand(querySelectUser, connection);
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                return reader;
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public string DeleteProduct()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(DefinirChemin());
+                string queryDeleteProduct = "DELETE FROM product where idProduct = 3";
+                MySqlCommand command = new MySqlCommand(queryDeleteProduct, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+            return "Votre produit à bien été supprimer !";
+        }
+
+        public string AddProduct(int number, string nameProduct)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(DefinirChemin());
+                string queryAddProduct = "INSERT INTO product (number,nameProduct) values(@number,@nameProduct)";
+                MySqlCommand command = new MySqlCommand(queryAddProduct, connection);
+                command.Parameters.AddWithValue("@number", number);
+                command.Parameters.AddWithValue("@nameProduct", nameProduct);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+
+            return "Votre produit à bien été ajouté dans la base de données";
+        }
+
+        public string ModifNameUserById(string name,int userId)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(DefinirChemin());
+                string queryNameModif = "UPDATE users SET name = @name WHERE idUser = @userId";
+                MySqlCommand command = new MySqlCommand(queryNameModif, connection);
+
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@userId", userId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+
+            return "Le nom de l'utilisateur à bien été modifié !";
+        }
+
+
+    }
+}
